6654f4b963bfe5613f124acf0484fa2a5f2318c6
RuMonty <world.ru.monty@gmail.com>
2020-03-23 19:01:59 +0300

    add walking and player .cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Walking _walking;
+    public Dictionary <Walking.WalkingDirection,KeyCode> _control;
+    void Start()
+    {
+        _control = new Dictionary<Walking.WalkingDirection, KeyCode>{       //андрей, это временный код, написал для теста\ примера, это управление ты должен мне 
+            {Walking.WalkingDirection.right, KeyCode.D},                    // предоставить из сцены старта игры.
+            {Walking.WalkingDirection.left, KeyCode.A},
+            {Walking.WalkingDirection.up, KeyCode.W},
+            {Walking.WalkingDirection.down, KeyCode.S},
+        };
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(_control[Walking.WalkingDirection.right]))
+            _walking.WalkingRight();
+        else if (Input.GetKeyDown(_control[Walking.WalkingDirection.left]))
+            _walking.WalkingLeft();
+        else if (Input.GetKeyDown(_control[Walking.WalkingDirection.up]))
+            _walking.WalkingUp();
+        else if (Input.GetKeyDown(_control[Walking.WalkingDirection.down]))
+            _walking.WalkingDown();
+    }
+}
---
+++ Assets/Scripts/Walking.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+
+    Vector3 _V3Left = new Vector3(-0.2f, 0, 0);
+    Vector3 _V3Right = new Vector3(0.2f, 0, 0);
+    Vector3 _V3Up = new Vector3(0, 0.2f, 0);
+    Vector3 _V3Down = new Vector3(0, -0.2f, 0);
+
+    Transform _lastPosition;
+    WalkingDirection _direction;
+    void Start()
+    {
+        _direction = WalkingDirection.idle;
+    }
+
+    void Walk()
+    {
+        switch (_direction)
+        {
+            case WalkingDirection.left:
+                WalkingLeft();
+                break;
+            case WalkingDirection.right:
+                WalkingRight();
+                break;
+            case WalkingDirection.up:
+                WalkingUp();
+                break;
+            case WalkingDirection.down:
+                WalkingDown();
+                break;   
+            default:
+                return;
+        }
+    }
+
+    public void WalkingLeft() => transform.localPosition += _V3Left;
+    public void WalkingRight() => transform.localPosition += _V3Right;
+    public void WalkingUp() => transform.localPosition += _V3Up;
+    public void WalkingDown() => transform.localPosition += _V3Down;
+
+    public enum WalkingDirection
+    {
+        left, right, up, down, idle
+    }
+}
