1dc9af2515f7c6e6f9d6ae578f52f17978981837
RuMonty <world.ru.monty@gmail.com>
2020-05-05 09:29:56 +0300

    BorderBlock refresh CheckWalk

---
+++ Assets/Scripts/BorderBlock.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderBlock : MonoBehaviour
+{
+    List<Coordinates> listOfCoordinates;
+    [SerializeField]
+    List<GameObject> linkBorders;
+    struct Coordinates
+    {
+        int x,y;
+        internal float X
+        {
+            get{
+                return x;
+            }
+            set{
+               x = (int) value;     
+            }
+        }
+        internal float Y
+        {
+            get{
+                return y;
+            }
+            set{
+               y = (int) value;     
+            }
+        }
+    }
+    void Start()
+    {
+        listOfCoordinates = new List<Coordinates>();
+        Coordinates startPos;
+        foreach (GameObject gameObj in linkBorders)
+        {
+            startPos = new Coordinates{ X = gameObj.transform.localPosition.x, Y = gameObj.transform.localPosition.y};
+            Filling(gameObj.GetComponentsInChildren<Transform>(), startPos, gameObj.transform);                                   
+        }
+    }    
+    void Filling(Transform[] roughCopy, Coordinates startPos, Transform gameObj)
+    {
+        foreach (Transform trans in roughCopy)
+        {
+            int rotation = ((int) gameObj.localEulerAngles.z) % 360;
+            Coordinates tmpPos = new Coordinates();
+            if (rotation == 0)
+                tmpPos = new Coordinates{X = startPos.X + trans.localPosition.x , Y = startPos.Y + trans.localPosition.y};
+            else if (rotation == 90 || rotation == -270)                
+                tmpPos = new Coordinates{X = startPos.X - trans.localPosition.y , Y = startPos.Y + trans.localPosition.x};
+            else if (rotation == -90 || rotation == 270)
+                tmpPos = new Coordinates{X = startPos.X + trans.localPosition.y , Y = startPos.Y - trans.localPosition.x};
+            else if (rotation == -180 || rotation == 180) 
+                tmpPos = new Coordinates{X = startPos.X - trans.localPosition.x , Y = startPos.Y - trans.localPosition.y};
+            if (gameObj != trans)
+                listOfCoordinates.Add(tmpPos);
+        }
+    }
+    internal bool CheckWalk(Vector3 position, Vector3 direction)           //тут ошибка с расчетом int x y
+    {
+        Debug.Log(direction.ToString());
+        bool rezult = true;
+        int x = (int) position.x;
+        int y = (int) position.y;
+
+        if (Mathf.Abs(direction.x) > 0)        
+            x = (direction.x > 0 && position.x > 0 || direction.x < 0 && position.x < 0) ? (int) (position.x + direction.x) : (int) position.x;     
+        else if (Mathf.Abs(direction.y) > 0)        
+            y = (direction.y > 0 && position.y > 0 || direction.y < 0 && position.y < 0) ? (int) (position.y + direction.y) : (int) position.y;      
+        Coordinates tmpPos = new Coordinates{X = x, Y =  y};
+        foreach (var item in listOfCoordinates)
+        {
+            if (item.X == tmpPos.X && item.Y == tmpPos.Y)
+                rezult = false;
+        }
+        Debug.Log("Проверяется позиция: " + x + " : " + y +". вектор3: " + position.x + " : " + position.y+ ". результат: "+ rezult.ToString());
+        return rezult;
+    }
+}
--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -13,20 +13,17 @@ public class Walking : MonoBehaviour
     [SerializeField]
     Rigidbody2D _rigidBody2D;                      // ссылка необходимая для управления движением
     [SerializeField]
-    float fallspeed;                                // скорость пакмана
-    [SerializeField]
-    float fallspeedcheck;
+    float fallspeed;                                // скорость пакмана    
     [SerializeField]
     float changePos;                                //единица шага пакмана (влияет на скороть передвижения)
 
     Vector3 _lastPosition;                          //нужэно для сохранения позиции(пока не используется)
-    bool checkedChangeDirection;
     int countExistenceChangeDirection;
 
     WalkingDirection _direction;                    //нужно для сохранения направления движения
     WalkingDirection changeDirection;               //смена направления движения(пока не используется)
 
-    float fall, fallcheck;                                     //переменная для сохранения времени Time.time
+    float fall;                                     //переменная для сохранения времени Time.time
 
     System.Func<Vector3, Vector2> goAhead;          //идти вперед, делегат для сохранения метода движения
     System.Func<Vector3, Vector2> goSide;           //идти назад(пока не используется)
@@ -39,122 +36,32 @@ public class Walking : MonoBehaviour
     void Start()                                    //вызывается на старте игры
     {        
         countExistenceChangeDirection = 0;
-        checkedChangeDirection = false;
-        fall = 0; fallcheck = 0;                  
+        fall = 0;                  
         _direction = WalkingDirection.idle;         //присваеваем на старте стоять на месте
         changeDirection = WalkingDirection.idle;
         _V3Left = new Vector3(-changePos, 0, 0);
         _V3Right = new Vector3(changePos, 0, 0);
         _V3Up = new Vector3(0, changePos, 0);
         _V3Down = new Vector3(0, -changePos, 0);        
-    }   
-    void FixedUpdate()
-    {           
-        if (Time.time - fall >= fallspeed && _direction != WalkingDirection.idle)        
-        {         
-            _lastPosition = transform.localPosition;
-            if (changeDirection != WalkingDirection.idle && IntegerCheck(_lastPosition))
-            {                
-                if (countExistenceChangeDirection <= 1)
-                {                    
-                    Debug.Log("shag vstorony "+countExistenceChangeDirection.ToString());                    
-                    checkedChangeDirection = true;                   
-                    _rigidBody2D.MovePosition(goSide(AlignPosition(changeDirection)));
-                    countExistenceChangeDirection++;                    
-                }
-                else
-                {
-                    checkedChangeDirection = false;
-                    countExistenceChangeDirection = 0;
-                    changeDirection = WalkingDirection.idle;
-                }
-            }                               
-            else             
-            {
-                _rigidBody2D.MovePosition(goAhead(transform.localPosition));   
-                //Debug.Log("Shag");    
-            }     
-            fall = Time.time;
-        }
-    }
-    void Update() // нужно попробовать обойтись без этого
-    {
-        if (checkedChangeDirection /* && IntegerCheck(transform.localPosition)*/ && changeDirection == CheckedWalk() && Time.time - fallcheck >= fallspeedcheck)   //должен срабатывать только в случае успешного поворота
-        {               
-            //Debug.Log("true");                        //при смене позиции добавить выравнивание по осям x y, в зависимости от направления движения
-            RefreshDirection();
-            fallcheck = Time.time;
-        }     
-    }    
-    void RefreshDirection()
-    {
-        transform.localPosition = AlignPosition(changeDirection);
-        _direction = changeDirection;
-        goAhead = goSide;
-        changeDirection = WalkingDirection.idle;
-        checkedChangeDirection = false;             //
-        countExistenceChangeDirection = 0;        
-    }    
-    Vector3 AlignPosition(WalkingDirection dir)
-    {
-        Vector3 pos = transform.localPosition;
-        return (dir == WalkingDirection.up || dir == WalkingDirection.down) ?
-        new Vector3(Mathf.Round(pos.x), pos.y, pos.z) : new Vector3(pos.x, Mathf.Round(pos.y), pos.z);
-    }
-    void OnTriggerEnter2D(Collider2D other)                                 //событие возникающее при собрикосновении boxcollider и пакмана (стены)
-    {
-        Debug.Log("Triger " + other.name);
-        if ( other.name.IndexOf("Point") >= 0 )
-            return;        
-        if (checkedChangeDirection)
-        {
-            transform.localPosition = _lastPosition;
-            checkedChangeDirection = false;
-            //_rigidBody2D.MovePosition(goAhead(transform.localPosition));
-            transform.localPosition = goAhead(_lastPosition);
-        }
-        else
-        {
-            changeDirection = WalkingDirection.idle;
-            _direction = WalkingDirection.idle;                                  // направление изменяем на значение "стоять на месте"
-            transform.localPosition = RoundPosition(_lastPosition);   
-        }
-    }        
-    WalkingDirection CheckedWalk()                      // возвращает фактическое направление движения
-    {        
-        Vector3 tmp = transform.localPosition - _lastPosition;
-        //Debug.Log("CheckWalk tmp= " + tmp.x.ToString() + "; " + tmp.y.ToString());
-        if (Mathf.Abs(tmp.x) > Mathf.Abs(tmp.y))
-        {
-            if (tmp.x > 0)        
-                return WalkingDirection.right; 
-            else if (tmp.x < 0)        
-                return WalkingDirection.left; 
-        }
-        else 
-        {
-            if (tmp.y > 0)
-                return WalkingDirection.up;
-            else if (tmp.y < 0)
-                return WalkingDirection.down;
-        }
-        return WalkingDirection.idle;
-    }    
+    }  
     bool IntegerCheck(Vector3 pos)          // проверка позиции объекта, при значениях близких к целым (~turnBondary) возвращает истину
     {
         float axisX = (int)(Mathf.Abs(pos.x)*100) % 100;
         float axisY = (int)(Mathf.Abs(pos.y)*100) % 100;
-        //Debug.Log("tmpINT Position X = " + tmpInt.X + ", Y = " + tmpInt.Y);
-        int turnB1 = turnBoundary;             
-        int turnB2 = 100 - turnBoundary;    
-        if((axisX < turnB1 || axisX > turnB2) && (axisY < turnB1 || axisY > turnB2))
+
+        int turnB11 = (int) (changePos*100) / 3;        //5
+        int turnB12 = 100 - (int) (changePos*100) + turnB11;  //90
+        if((axisX < turnB11 || axisX >= turnB12) && (axisY < turnB11 || axisY >= turnB12))
             return true;
-        //Debug.Log("IntegerCheck = False");
         return false;
+    }
+    Vector3 AlignPosition(Vector3 pos, WalkingDirection dir)
+    {
+        return (dir == WalkingDirection.up || dir == WalkingDirection.down) ?
+        new Vector3(Mathf.Round(pos.x), pos.y, pos.z) : new Vector3(pos.x, Mathf.Round(pos.y), pos.z);
     }
     internal void DirectionSelection(WalkingDirection direction)
     {
-        //Debug.Log("DirectionSelection");
         if (direction == _direction)                            
             return;
         if(_direction == WalkingDirection.idle)
@@ -164,14 +71,12 @@ public class Walking : MonoBehaviour
         }
         else
         {
-            //Debug.Log("change directoin");
             if (changeDirection != direction)
             {
-                checkedChangeDirection = false;
+                //checkedChangeDirection = false;
                 changeDirection = direction;
                 goSide = SwitchDirection(changeDirection);
             }
-            //Debug.Log(goSide.Target.ToString());
             countExistenceChangeDirection = 0;            
         }
     }
@@ -203,4 +108,73 @@ public class Walking : MonoBehaviour
     Vector2 WalkDown(Vector3 position) => (Vector2)(position + _V3Down);
     Vector3 RoundPosition(Vector3 pos) => new Vector3(Mathf.Round(pos.x), Mathf.Round(pos.y), pos.z);     //метод принимает значение позиции и возращает его с округленными до целых
     
+    [SerializeField]
+    BorderBlock _borderBlock;
+
+    void Update()
+    {
+        if (Time.time - fall >= fallspeed && _direction != WalkingDirection.idle)        
+        {         
+            Vector3 updatePosition = transform.localPosition;
+            Vector3 tempPosition;
+            bool checkGoSide = false;
+            if (changeDirection != WalkingDirection.idle && IntegerCheck(updatePosition))
+            {                
+                if (countExistenceChangeDirection <= 1)
+                {
+                    tempPosition = goSide(AlignPosition(updatePosition, changeDirection));
+                    if (_borderBlock.CheckWalk(tempPosition, VectorDirectiod(changeDirection)))
+                    {
+                        updatePosition = tempPosition;
+                        checkGoSide = true;  
+                    }
+                    countExistenceChangeDirection++;
+                }
+                else                                    
+                    NullifyTurn();                 
+            }                               
+            if (!checkGoSide)           
+            {        
+                tempPosition = goAhead(updatePosition);        
+                if (_borderBlock.CheckWalk(tempPosition, VectorDirectiod(_direction)))
+                    updatePosition = tempPosition;  
+                else
+                {
+                    _direction = WalkingDirection.idle;
+                    NullifyTurn(); 
+                    updatePosition = new Vector3(Mathf.Round(updatePosition.x), Mathf.Round(updatePosition.y), updatePosition.z);
+                }
+            }
+            else
+            {
+                _direction = changeDirection;
+                goAhead = goSide;
+                NullifyTurn(); 
+            }
+
+            transform.localPosition = updatePosition;
+            fall = Time.time;
+        }
+    }
+    void NullifyTurn()
+    {
+        changeDirection = WalkingDirection.idle;  
+        countExistenceChangeDirection = 0;  
+    }
+
+    Vector3 VectorDirectiod(WalkingDirection dir)
+    {
+        Vector3 tmp;
+        if (dir == WalkingDirection.up)
+            tmp = Vector3.up;
+        else if (dir == WalkingDirection.down)
+            tmp = Vector3.down;
+        else if (dir == WalkingDirection.left)
+            tmp = Vector3.left;
+        else if (dir == WalkingDirection.right)
+            tmp = Vector3.right;
+        else
+            tmp = Vector3.zero;
+        return tmp;
+    }
 }
